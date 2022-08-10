5c11bfad856ca3b6e9d6feed7fd77feeeaf5e9c4
Anna Danilenko <danilenko.anna2016@yandex.ru>
2022-08-10 18:52:03 +0300

    feat: movies layout 1280

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
 
 function App() {
   return (
@@ -11,6 +12,11 @@ function App() {
         <Route exact path='/'>
           <Header isLoggedIn={false} />
           <Main />
+          <Footer />
+        </Route>
+        <Route path='/movies'>
+          <Header isLoggedIn={true} />
+          <Movies />
           <Footer />
         </Route>
       </Switch>
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,20 @@
+import './Movies.css';
+import SearchForm from '../SearchForm/SearchForm';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import movies from '../../utils/MoviesArray';
+
+function Movies() {
+  return (
+    <main className='movies-page'>
+      <SearchForm />
+      <MoviesCardList movies={movies} />
+      <div className='movies__button-more'>
+        <button className='movies__button' type='button' name='more'>
+          Ещё
+        </button>
+      </div>
+    </main>
+  );
+}
+
+export default Movies;
